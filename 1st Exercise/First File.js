bfdceec336b121952417457df0041c39b472b39d
mbarbour0 <matthew.barbour@icloud.com>
2018-04-14 13:49:15 -0400

    Updates

---
+++ 1st Exercise/First File.js
@@ -0,0 +1,18 @@
+var a = 5;
+var b = 4;
+var theNumbersMatch;
+
+if ( a == b ) {
+  theNumbersMatch = true;
+} else {
+  theNumbersMatch = false;
+}
+
+console.log("the numbers match: "+theNumbersMatch);
+
+var pens = ['red', 'blue', 'green', 'orange'];
+var mixedBag = ['keys', 5, false, 568, 'one more thing'];
+mixedBag[3] = true
+
+console.log(pens);
+console.log(mixedBag);
